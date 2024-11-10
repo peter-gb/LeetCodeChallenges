00e88be5cf84f4e345f26c32ae04f13141721cb0
peter-gb <43183973+peter-gb@users.noreply.github.com>
2024-11-10 08:25:02 +0000

    Time: 329 ms (99.48%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,28 @@
+/* 47 SQL 50 */
+
+-- Summarise all salaries
+with salaries as (
+    select distinct salary
+    from employee
+)
+-- Rank all the salaries
+, ranks as (
+    select 
+     salary
+     ,rank() over ( order by salary desc) as rank
+    from salaries
+)
+-- Provide a row for null if there is no rank 2
+, scaffold as (
+    select top (1) 2 as nullrank from employee
+)
+ , final as (-- find the second highest, null if none
+select r.salary as SecondHighestSalary
+from ranks r
+full outer join scaffold s
+on r.rank = s.nullrank
+where nullrank = 2
+ )
+
+-- deal with having an empty table in the original table... getting annoying now!
+ select max(SecondHighestSalary) as SecondHighestSalary from final
